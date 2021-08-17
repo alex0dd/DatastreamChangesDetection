89c4a04162ed2ff1e356388acfe793f2f0d27a76
Alex P <alexpod1000@gmail.com>
2021-08-17 22:56:55 +0200

    Extended synthetic data generation to multidimensional data.

--- utils/synthetic_data.py
+++ utils/synthetic_data.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-def generate_synthetic_normal(locs, sizes, scale=1.0):
+def generate_synthetic_normal(locs, sizes, scale=1.0, dims=1):
     assert len(locs) == len(sizes)
     streams = []
     for loc, size in zip(locs, sizes):
-        streams.append(np.random.normal(loc=loc, scale=scale, size=size))
+        streams.append(np.random.normal(loc=loc, scale=scale, size=(size, dims)))
     return np.concatenate(streams)
