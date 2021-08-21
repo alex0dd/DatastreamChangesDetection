66ccae21b42687d2d0225d129961d25e31eb08f5
Alex P <alexpod1000@gmail.com>
2021-08-21 22:51:35 +0200

    Added scale parameter to synthetic data creator function.

--- utils/synthetic_data.py
+++ utils/synthetic_data.py
@@ -2,7 +2,11 @@ import numpy as np
 
 def generate_synthetic_normal(locs, sizes, scale=1.0, dims=1):
     assert len(locs) == len(sizes)
+    if type(scale) == list and len(scale) != len(sizes):
+        raise ValueError("scale and sizes lists must have equal length.")
+    elif type(scale) in (int, float):
+        scale = [scale]*len(sizes)
     streams = []
-    for loc, size in zip(locs, sizes):
-        streams.append(np.random.normal(loc=loc, scale=scale, size=(size, dims)))
+    for loc, scal, size in zip(locs, scale, sizes):
+        streams.append(np.random.normal(loc=loc, scale=scal, size=(size, dims)))
     return np.concatenate(streams)
